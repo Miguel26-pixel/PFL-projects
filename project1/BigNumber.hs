42a80bf741f8344d847e818fe4841decdac99203
NanD0245 <fernandoluisrego@gmail.com>
2021-11-12 19:26:39 +0000

    done exercise 2

--- project1/BigNumber.hs
+++ project1/BigNumber.hs
@@ -1,35 +1,75 @@
-type BigNumber = [Int]
+type BigNumber = (Bool, [Int]) -- True -> positivo ; False -> negativo
 
 --'0' = 48
 
 scanner :: String -> BigNumber
-scanner s = [fromEnum x - 48 | x <- s]
+scanner s   | head s == '-' = (False ,[fromEnum x - 48 | x <- reverse (tail s)])
+            | otherwise = (True, [fromEnum x - 48 | x <- reverse s])
+
 
 output :: BigNumber -> String
-output a = [toEnum (x + 48)::Char | x <- a]
+output a    | fst a = [toEnum (x + 48)::Char | x <- reverse (snd a)]
+            | otherwise = '-' : [toEnum (x + 48)::Char | x <- reverse (snd a)]
 
-somaBN :: BigNumber -> BigNumber -> BigNumber --assumindo que números estão ao contrário
-somaBN a [] = a
-somaBN [] b = b
-somaBN (x:xs) (y:ys)    | x + y > 9 = mod (x + y) 10 : somaBN (if null xs then [1] else (1 + head xs) : tail xs) ys --números positivos
-                        | otherwise = x + y : somaBN xs ys
+
+somaBN :: BigNumber -> BigNumber -> BigNumber
+somaBN a b  | fst a && fst b = (True, somaArray (snd a) (snd b) 0)
+            | fst a && not (fst b) = subBN a b
+            | not (fst a) && fst b = subBN a b
+            | otherwise = (False, somaArray (snd a) (snd b) 0)
+
+
+somaArray :: [Int] -> [Int] -> Int -> [Int]
+somaArray [] [] c = [] -- [0]
+somaArray a [] c = a
+somaArray [] b c = b
+somaArray (x:xs) (y:ys) c   | x + y + c > 9 = mod (x + y + c) 10 : somaArray (if null xs then [0] else xs) (if null ys then [0] else ys) 1--números positivos
+                            | otherwise = x + y + c: somaArray xs ys 0
+
+--99 2 0 -> 9+2=11>9 -> 1 :
+--[9] [0] 1 -> 0 : [0] [0] 1
+
+maiorOuIgualModulo :: BigNumber -> BigNumber -> Bool
+maiorOuIgualModulo (_,[]) (_,[]) = True
+maiorOuIgualModulo (a,x) (b,y)  | length x > length y = True --errado por causa dos zeros
+                                | length y > length x = False --errado por causa dos zeros
+                                | otherwise = if last x /= last y then last x >= last y else maiorOuIgualModulo (a,init x) (b, init y)
+
 
 subBN :: BigNumber -> BigNumber -> BigNumber --maior em primeiro'
-subBN a [] = a
-subBN [] b = b
-subBN (x:xs) (y:ys) | x < y = x + 10 - y : subBN ((head xs - 1) : tail xs) ys
-                    | otherwise = x - y : subBN xs ys
+subBN a (_,[]) = a
+subBN (_,[]) (b,y) = (not b,y)
+subBN a b   | fst a && fst b = if maiorOuIgualModulo a b then (True, limpaZeros (subArray (snd a) (snd b) 0)) else (False, limpaZeros (subArray (snd b) (snd a) 0))
+            | fst a && not (fst b) = somaBN a b
+            | not (fst a) && fst b = (False, somaArray (snd a) (snd b) 0)
+            | otherwise = if maiorOuIgualModulo b a then (True, limpaZeros (subArray (snd b) (snd a) 0)) else (False, limpaZeros (subArray (snd a) (snd b) 0))
+
+
+subArray :: [Int] -> [Int] -> Int -> [Int]
+subArray a [] c = a
+subArray [] b c = b
+subArray (x:xs) (y:ys) c    | x < y + c = x + 10 - y - c : subArray (if null xs then [0] else xs) (if null ys then [0] else ys) 1
+                            | otherwise = x - y - c: subArray xs ys 0
+--[0,0,1] [2] 0 -> 8 : [0,1] [0] 1 
+--[0,1] [0] 1 -> 9 : [1] [0] 1
+
+limpaZeros :: [Int] -> [Int]
+limpaZeros l = reverse (until (\x -> (x == [0]) || (head x /= 0)) tail (reverse l))
+
 
 mulBN :: BigNumber -> BigNumber -> BigNumber
-mulBN a [] = []
-mulBN [] b = []
-mulBN (x:xs) (y:ys) = somaBN (mulBNCarry (x:xs) [y] 0) (0 : mulBNCarry (x:xs) ys 0)
+mulBN a (_,[]) = (True,[0])
+mulBN (_,[]) b = (True,[0])
+mulBN (a,x) (b,y:ys) = (not a /= b, somaArray (mulBNCarry x [y] 0) (0 : snd (mulBN (a,x) (b,ys))) 0)
 
-mulBNCarry :: BigNumber -> BigNumber -> Int -> BigNumber
+
+mulBNCarry :: [Int] -> [Int] -> Int -> [Int]
 mulBNCarry a [] c = []
 mulBNCarry [] b c = []
 mulBNCarry (x:xs) (y:ys) c  | x * y + c > 9 = mod (x * y + c) 10 : mulBNCarry (if null xs then [0] else head xs : tail xs) [y] (div (x*y+c) 10)
                             | otherwise = x * y + c : mulBNCarry xs [y] 0
 
---divBN :: BigNumber -> BigNumber -> (BigNumber, BigNumber)
---divBN a b = until (\(x,y) -> y == 0) (\(x,y) -> (y,mod x y)) (a,b)
+divBN :: BigNumber -> BigNumber -> (BigNumber,BigNumber)
+divBN a b = last (until (\[(w,x),(y,z)] -> not (fst (subBN z x))) (\[(w,x),(y,z)] -> [(w,x),(somaBN y (True,[1]), subBN z x)]) [(a,b),((True,[0]),a)])
+
+
