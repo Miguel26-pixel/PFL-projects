bd1421364b0eadda0d2c257297ef4fe82c58176e
NanD0245 <fernandoluisrego@gmail.com>
2022-01-19 11:10:06 +0000

    add sleep and clear window

--- prologProject/src/pathway.pl
+++ prologProject/src/pathway.pl
@@ -1,6 +1,7 @@
 :- include('game.pl').
 :- include('view.pl').
 :- use_module(library(random)).
+:- use_module(library(system)).
 
 /*initial_state(Board/_Turn/_Players) :-  Board = [   [0,0,0,0,0,0],
                                                     [0,0,0,0,0,0],
@@ -23,7 +24,10 @@ initial_state(Board/_Turn/_Players) :-  Board = [   [2,0,2,1,0,2],
                                                     [0,1,0,2,2,0],
                                                     [1,1,0,0,0,0]   ].
 
-play :- menu(Option),
+clear :- write('\33\[2J').
+
+play :- clear,
+        menu(Option),
         play(Option). 
 
 menu(Option) :- display_menu,
@@ -39,6 +43,7 @@ playPvP :-  initial_state(Board/Turn/Players),
             Turn is 1,
             get_names(P1,P2),
             Players = P1/P2,
+            clear,
             display_game(Board/Turn/Players),
             playPvP(Board/Turn/Players).
 
@@ -50,6 +55,7 @@ playPvP(GameState) :-   game_over(GameState,Winner),
 
 playPvP(GameState) :-   choose_move_human(GameState,Move),
                         move(GameState,Move,NewGameState),
+                        clear,
                         display_game(NewGameState),
                         playPvP(NewGameState).
 
@@ -57,6 +63,7 @@ playPvC :-  initial_state(Board/Turn/Players),
             Turn is 1,
             get_name_computer(P),
             Players = P/"Computer",
+            clear,
             display_game(Board/Turn/Players),
             playPvsC(Board/Turn/Players).
 
@@ -64,6 +71,7 @@ playCvP :-  initial_state(Board/Turn/Players),
             Turn is 2,
             get_name_computer(P),
             Players = P/"Computer",
+            clear,
             display_game(Board/Turn/Players),
             playPvsC(Board/Turn/Players).
 
@@ -76,17 +84,21 @@ playPvsC(GameState) :-  game_over(GameState,Winner),
 playPvsC(GameState) :-  is_human(GameState),
                         choose_move_human(GameState,Move),
                         move(GameState,Move,NewGameState),
+                        clear,
                         display_game(NewGameState),
                         playPvsC(NewGameState).
 
 playPvsC(GameState) :-  choose_move(GameState, 1, Move),
                         move(GameState,Move,NewGameState),
+                        sleep(0.5),
+                        clear,
                         display_game(NewGameState),
                         playPvsC(NewGameState).
 
 playCvC :-  initial_state(Board/Turn/Players),
             Turn is 1,
             Players = "Computer-1"/"Computer-2",
+            clear,
             display_game(Board/Turn/Players),
             playCvsC(Board/Turn/Players).
 
@@ -98,5 +110,7 @@ playCvsC(GameState) :-  game_over(GameState,Winner),
 
 playCvsC(GameState) :-  choose_move(GameState, 1, Move),
                         move(GameState,Move,NewGameState),
+                        sleep(0.5),
+                        clear,
                         display_game(NewGameState),
                         playCvsC(NewGameState).
